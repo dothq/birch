92d29d26951987ae35647249e9b7794399d8e70d
EnderDev <xenderpugx@gmail.com>
2020-05-30 17:31:17 +0100

    🔥 Remove triangle because it broke some things

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const c = require("colors");
 const getType = () => {
     const isR = (process && process.type === 'renderer');
 
-    return c.bold.blue(`⯅  ${isR ? "Renderer" : "Main"}`)
+    return c.bold.blue(`${isR ? "Renderer" : "Main"}`)
 }
 
 export const log = (payload) => {
